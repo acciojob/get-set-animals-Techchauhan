efa17543b1d667a5fa8c0096dcd27b78813775fc
Techchauhan <88832979+Techchauhan@users.noreply.github.com>
2024-12-05 23:46:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-//complete this code
 class Animal {
-	contructor(species){
+	constructor(species){
 		this._species = species;
 	}
 
 	get species(){
-		return this._species
+		return this._species;
 	}
 
 	makeSound(){
-		 console.log(`The ${this._species} makes a sound.`);
+		console.log(`The ${this._species} makes a sound.`);
 	}
-	 
-
-	
 }
 
 class Dog extends Animal {
 	bark(){
-		 console.log("woof");
+		console.log("woof");
 	}
 }
 
 class Cat extends Animal {
 	purr() {
-    console.log("purr");
-  }
+		console.log("purr");
+	}
 }
 
 // Do not change the code below this line
